c12b4c9bdaa39b7299c90cb50adb97ac61fb685f
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-06 23:54:53 +0700

    DungNguyen

---
+++ Bai19_FontDialog/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Bai19_FontDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.txtText = new System.Windows.Forms.TextBox();
+			this.btnChonFont = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtText
+			// 
+			this.txtText.Location = new System.Drawing.Point(37, 39);
+			this.txtText.Name = "txtText";
+			this.txtText.Size = new System.Drawing.Size(575, 34);
+			this.txtText.TabIndex = 0;
+			// 
+			// btnChonFont
+			// 
+			this.btnChonFont.Location = new System.Drawing.Point(193, 109);
+			this.btnChonFont.Name = "btnChonFont";
+			this.btnChonFont.Size = new System.Drawing.Size(265, 48);
+			this.btnChonFont.TabIndex = 1;
+			this.btnChonFont.Text = "Chọn &font";
+			this.btnChonFont.UseVisualStyleBackColor = true;
+			this.btnChonFont.Click += new System.EventHandler(this.btnChonFont_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightGreen;
+			this.ClientSize = new System.Drawing.Size(650, 176);
+			this.Controls.Add(this.btnChonFont);
+			this.Controls.Add(this.txtText);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.Red;
+			this.Margin = new System.Windows.Forms.Padding(5);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Bài 19: Font Dialog";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnChonFont;
+    }
+}
+
---
+++ Bai19_FontDialog/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai19_FontDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChonFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.ShowColor = true;    // Hiển thị bảng Color
+            if(fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtText.Font = fontDialog.Font; // Lấy font đã chọn từ hộp thoại font
+                txtText.ForeColor = fontDialog.Color;   // Lấy màu đã chọn từ bảng color
+            }    
+        }
+    }
+}
